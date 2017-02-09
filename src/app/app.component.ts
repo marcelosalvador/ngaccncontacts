d98ffe5ef547e3bf15392bd6a36a9e62a9ee2ff5
Marcelo Salvador <c-marcelo.salvador@charter.com>
2017-02-09 16:53:00 -0500

    New component changes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
 
+export class Employee {
+  id: number;
+  name: string;
+  title: string;
+
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>Hello {{employee.title}}</h1>
+    <h2>{{employee.name}}</h2>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+  title = 'Software Engineering Manager';
+  name = 'Marcelo Salvador';
+  employee: Employee = {
+    id: 1,
+    name: 'Marcelo Salvador',
+    title: 'Software Engineering Manager'
+  }
+}
